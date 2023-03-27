e5a06739c9c615070fba8b7a7269f10150dd7a9e
LYN <leoyubneu4@gmail.com>
2023-03-27 22:36:08 +0200

    delet folder

--- web-projects/test-sites/database_first_thoughts.py
+++
@@ -1,8 +0,0 @@
-#Wich databses do I need for my web?
-
-class Article():
-  id = 'integer'     #the users ID 
-  title = 'string'   #title of the info I want to store
-  content = 'text'   #info I want to store about what appears on my web page
-  author = 'string'  #who saved the data + maybe when?
-  date = 'date'      #when the change happened + maybe why?
